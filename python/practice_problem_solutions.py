f934b593061f3927ad97e5d9e1161db1baa22f75
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2021-02-20 10:01:11 +0500

    Initial commit

---
+++ python/practice_problem_solutions.py
@@ -0,0 +1,61 @@
+#Write a program to print twinkle twinkle little star poem in python
+# print("My Solution")
+# print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+# print("Up above the world so high,\nLike a diamond in the sky.")
+# print("When the blazing sun is gone,\nWhen he nothing shines upon,")
+# print("Then you show your little light,\nTwinkle, twinkle, all the night.")
+# print("Then the traveler in the dark\nThanks you for your tiny spark,")
+# print("How could he see where to go,\nIf you did not twinkle so?")
+# print("In the dark blue sky you keep,\nOften through my curtains peep") 
+# print("For you never shut your eye,\nTill the sun is in the sky.")
+# print("As your bright and tiny spark\nLights the traveler in the dark,")
+# print("Though I know not what you are,\nTwinkle, twinkle, little star.")
+
+# print("\nHarry Solution")
+# print('''Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the traveler in the dark
+# Thanks you for your tiny spark,
+# How could he see where to go,
+# If you did not twinkle so?
+
+# In the dark blue sky you keep,
+# Often through my curtains peep
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# As your bright and tiny spark
+# Lights the traveler in the dark,
+# Though I know not what you are,
+# Twinkle, twinkle, little star.''')
+
+# Exercise 2:-Install any external module you like and perform any operation of that module
+# !pip install numpy
+# import numpy as np
+# np.array([1,2,3])
+
+#Exercise 3:-Print the contents of a directory using os module
+# print("Printing the files of current directory")
+# import os
+# file_names = os.listdir()
+# for files in file_names:
+#     print(files)
+
+#Exercise 4:-Label the above program with comments
+# #Here,We are printing in which directory we are applying our method
+# print("Printing the files of current directory")
+# import os #importing theos module
+# '''using os.listdir() method for getting the names of files in current directory'''
+# file_names = os.listdir() 
+# #Applying loop on file names got by above method and printing them
+# for files in file_names: 
+#     print(files)
+
